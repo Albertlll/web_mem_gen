482501811ce4cf752e691e593b8e0f7ac73f8bff
Albertlll <king.python@mail.ru>
2024-04-29 20:30:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [state, setState] = useState('null');
+
+  useEffect(() => {
+    fetch('/api')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data)
+      setState(data[0]);
+    });
+    }, [])
+
+  
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{state}</h1>
+      <div className="card">
+
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  build: {
+    assetsDir: 'static',
+  },
+  server: {
+    port: 3000,
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
