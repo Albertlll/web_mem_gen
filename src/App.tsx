ec5f8eda5c48cd848d3c5b15166fea710c618d69
Albertlll <king.python@mail.ru>
2024-07-12 03:50:22 +0300

    полностью рабочий трансформер

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 // import { Button } from "@/components/ui/button"
@@ -12,21 +12,46 @@ import ImageInputBtn from "./components/shared/ImageInputBtn"
 import { Button } from "@/components/ui/button"
 // import { ScrollArea } from "@/components/ui/scroll-area"
 // import { KonvaEventObject } from "konva/lib/Node"
-
-import { Text } from "konva/lib/shapes/Text"
+import { Node } from "konva/lib/Node"
+import { Text } from "react-konva"
 import { Layer as La } from "konva/lib/Layer"
 import { Stage as St} from "konva/lib/Stage"
 import { Transformer as Tr } from "konva/lib/shapes/Transformer"
 import { Text as Te } from "konva/lib/shapes/Text"
+import TextField from "./components/shared/TextField"
+import { Ref } from "react";
+import { TextConfig } from "konva/lib/shapes/Text";
+
+
+
+interface TextFieldRrops extends TextConfig {
+}
+
 export function CardWithForm() {
   const [userBaseImage, setUserBaseImage] = useState<string | null>(null)
   const [memSize, setMemSize] = useState<{width : number, height: number}>({width: 500, height: 500})
   const [scale, setScale] = useState<Vector2d>({x: 1, y: 1})
-
   const [currrentTool, setCurrentTool] = useState<string | undefined>(undefined)
+  const [selectedText, setSelectedText] = useState<number | null>(null)
+  const [text, setText] = useState<string>("")
+  const [textFields, setTextFields] = useState<Array<TextFieldRrops>>([])
+
   const stageRef = useRef<St | null>(null) 
   const textLayerRef = useRef<La | null>(null) 
   const transformerRef = useRef<Tr | null>(null) 
+
+
+
+  useEffect(() => {
+    console.log(selectedText)
+
+
+    if (selectedText != null && transformerRef.current != null && textLayerRef.current) {
+      transformerRef.current.nodes([textLayerRef.current.getChildren()[selectedText]])
+    }
+
+  }, [selectedText])
+
 
   const handleSelectBaseImage = (imageList : FileList | null) => {
     if(imageList && imageList[0]) {
@@ -49,21 +74,26 @@ export function CardWithForm() {
   
   }
 
-  const textClick = (e : any) => {
-    console.log('textClick');
-    if (!transformerRef.current) return
-    transformerRef.current.nodes([e.target])
+  const textClick = (e : any, index : number) => {
+    // const index = e.target.index
+
+    // setTextFields([...textFields.slice(0, index), {...textFields[index], selected : true} ,...textFields.slice(index + 1)])
+    setSelectedText(index)
+    stageRef.current?.draw()
+    
   }
 
 
 
-
+  const removeTransformer = () => {
+    setSelectedText(null);
+  }
 
   const stageClick = (e : any) => {
-    console.log(currrentTool)
     switch (currrentTool) {
       case '':
-        // removeTransformer();
+        
+        selectedText || selectedText == 0 && removeTransformer();
         break;
       case "text":
         createClick(e)
@@ -87,8 +117,7 @@ export function CardWithForm() {
   const createClick = (e : any) => {
     const stage = e.target.getStage()
     const pos = stage.getPointerPosition()
-
-    const newText = new Text({
+    setTextFields([...textFields, {
       x: pos.x / scale.x,
       y: pos.y / scale.y,
       fill: "red",
@@ -98,15 +127,8 @@ export function CardWithForm() {
       fontFamily: "Arial",
       text: "Одобрено",
       draggable: true,
-      
-    })
-
-    newText.on("click", (event) => {textClick(event)})
-
-    if (!textLayerRef.current) return
-
-    textLayerRef.current.add(newText)
-    stage.draw()
+      selected: false,
+      }])
   }
   
 
@@ -161,9 +183,22 @@ export function CardWithForm() {
           </Layer>
 
           <Layer ref={textLayerRef}>
-
-            <Transformer ref={transformerRef}/>
+            {textFields.map((item, index) => (
+              <>
+              <TextField
+                transformerRef={transformerRef}
+                key={index}
+                {...item}
+                selected={selectedText == index}
+                onClick={(ev : any) => {textClick(ev, index)}}/>
+              </>
+          
+            ))}
+            
+            
           </Layer>
+
+
           </Stage>
             <div className=" bg-background absolute right-5 flex flex-col justify-center h-1000 border-solid border-[1px] border-slate-500 p-5 rounded-sm">
             <ToolBar value={currrentTool} setCurrentTool={setCurrentTool}/>
---
+++ src/components/shared/TextField.tsx
@@ -0,0 +1,32 @@
+import { Text } from "react-konva";
+import { Text as TextClass } from "konva/lib/shapes/Text";
+import { TextConfig } from "konva/lib/shapes/Text";
+import { Transformer } from "react-konva";
+import { Transformer as TransformerClass } from "konva/lib/shapes/Transformer";
+import { Ref } from "react";
+
+
+interface TextFieldRrops extends TextConfig {
+    transformerRef : Ref<TransformerClass>;
+}
+
+
+
+function TextField(props : TextFieldRrops) {
+    return (
+        <>
+        <Text
+            {...props}
+        />
+
+        {props.selected &&
+        <Transformer ref={props.transformerRef}/>
+        }
+        
+        </>
+        
+
+    );
+}
+
+export default TextField;
